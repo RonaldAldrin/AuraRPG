51bef9c9f59671895344d8240daf638b854eadc3
RonaldAldrin <ronaldmaningat@gmail.com>
2024-11-06 11:14:14 +0800

    Add player controller class and input

---
+++ Source/AuraRPGv2/Public/PlayerController/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright Awie
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURARPGV2_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
